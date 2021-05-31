a4adce21181b2e9bf4ca01b2cf1cc7bac7773169
Itzae <itzaeg@gmail.com>
2021-05-31 16:22:37 -0500

    Create screen login

--- app/src/main/java/com/itgonca/rentit/MainActivity.kt
+++ app/src/main/java/com/itgonca/rentit/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.itgonca.rentit
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+import com.itgonca.rentit.utils.extension.hideStatusBar
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_login)
+        hideStatusBar()
     }
 }
---
+++ app/src/main/java/com/itgonca/rentit/utils/extension/Activity.kt
@@ -0,0 +1,13 @@
+package com.itgonca.rentit.utils.extension
+
+import android.os.Build
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+fun AppCompatActivity.hideStatusBar() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.setDecorFitsSystemWindows(false)
+    } else {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    }
+}
